5816f27de93f71ef8cb74fe08ea92a100e40dce4
Lukas Guhl <lukas.guhl@proton.me>
2026-04-12 22:47:56 +0200

    added some previously implicit declarations

--- differenzierung.F90
+++ differenzierung.F90
@@ -1,13 +1,14 @@
 program differenzieren
 
+use iso_fortran_env, only: dp => real64
+
 implicit none
 integer                 :: config_file
-real(8)                 :: x1, x2, resolution, a_tol_exp, r_tol_exp
-real(8), allocatable    :: grid(:), function_values(:), derivative(:), derivative_converged(:)
-logical                 :: converged
-integer(8)              :: tick_start, tick_end, tick_rate
+real(dp)                :: x1, x2, resolution, a_tol_exp, r_tol_exp
+real(dp), allocatable   :: grid(:), function_values(:), derivative(:), derivative_converged(:)
+integer(8)              :: tick_start, tick_end, tick_rate, config_io
 
-open(newunit = config_file, file= "diff_config", status="old")
+open(newunit = config_file, file= "diff_config", status="old", iostat=config_io)
 
 read(config_file, *) x1
 read(config_file, *) x2
@@ -33,12 +34,13 @@ call central_difs(  y       = function_values, &
                     ! 15% faster as subroutine: Time per Grid Point:    2.1624915527673716E-008 as a function 
                     !                        vs Time per Grid Point:    1.7023371002457020E-008 as subroutine
 
-call converge_derivative(   grid_inout          = grid , &
-                            resolution_current  = resolution , &
-                            derivative_in       = derivative ,  &
-                            absolut_tol         = 10**(a_tol_exp) , &
-                            relative_tol        = 10**(r_tol_exp) , &
-                            derivative_out      = derivative_converged)
+call converge_derivative(   grid_inout           = grid , &
+                            function_values_inout= function_values ,&
+                            resolution_current   = resolution , &
+                            derivative_in        = derivative ,  &
+                            absolut_tol          = 10d0**(a_tol_exp) , &
+                            relative_tol         = 10d0**(r_tol_exp) , &
+                            derivative_out       = derivative_converged)
 
 
 
@@ -71,53 +73,53 @@ call quick_plot(grid, function_values, derivative_converged)
 
 contains 
 
-subroutine converge_derivative(grid_inout, resolution_current, derivative_in, absolut_tol, relative_tol, derivative_out)
+subroutine converge_derivative(grid_inout, function_values_inout, resolution_current, derivative_in, absolut_tol, relative_tol, derivative_out)
 
     implicit none
 
-    real(8), allocatable, intent(in)    :: derivative_in(:)    
-    real(8), allocatable, intent(inout) :: grid_inout(:) 
-    real(8), allocatable, intent(out)   :: derivative_out(:)
-    real(8), allocatable                :: derivative_working(:), derivative_old(:)
-    real(8), intent(inout)              :: resolution_current
-    integer                             :: j
-    real(8), intent(in)                 :: absolut_tol, relative_tol
+    real(dp), allocatable, intent(in)    :: derivative_in(:)    
+    real(dp), allocatable, intent(inout) :: grid_inout(:), function_values_inout(:)
+    real(dp), allocatable, intent(out)   :: derivative_out(:)
+    real(dp), allocatable                :: derivative_working(:), derivative_old(:)
+    real(dp), intent(inout)              :: resolution_current
+    integer                              :: j
+    real(dp), intent(in)                 :: absolut_tol, relative_tol
+    logical                              :: converged
     
 
     converged = .false.
     j = 0
-    !call move_alloc(from= derivative_in, to=derivative_working) !'from' argument of 'move_alloc' intrinsic at (1) cannot be INTENT(IN)
+
     derivative_working = derivative_in
 
     do while (.not. converged) 
         j= j+1
         if (j >= 20 .or. size(grid_inout) > 10**7) then
-            print *,  "maximum number of iterations reached"
+            !print *,  "maximum number of iterations reached"
             exit
         end if
 
         call move_alloc(from=derivative_working, to= derivative_old)
 
-        call double_grid_function(grid_inout, resolution_current, function_values) ! there is no noticable benefit moving the double_function back out into a function  
+        call double_grid_function(grid_inout, resolution_current, function_values_inout) ! there is no noticable benefit moving the double_function back out into a function  
 
-        call central_difs(  y       = function_values, &
+        call central_difs(  y       = function_values_inout, &
                             h       = resolution_current, &
                             y_prime = derivative_working)
 
         converged = check_convergence(derivative_old, derivative_working, absolut_tol, relative_tol)
-        print*, converged
     end do
 
     call move_alloc(from=derivative_working, to=derivative_out) 
 end subroutine converge_derivative
 
 subroutine double_grid_function(grid_inout, grid_resolution, f_inout)
-    real(8), allocatable, intent(inout)    :: grid_inout(:), f_inout(:)
-    real(8), allocatable                   :: grid_old(:), f_old(:)
+    real(dp), allocatable, intent(inout)    :: grid_inout(:), f_inout(:)
+    real(dp), allocatable                   :: grid_old(:), f_old(:)
 
-    !real(8), allocatable, intent(out)   :: grid_out(:)
-    real(8), intent(inout)              :: grid_resolution
-    real(8)                             :: x_start, x_end
+    !real(dp), allocatable, intent(out)   :: grid_out(:)
+    real(dp), intent(inout)              :: grid_resolution
+    real(dp)                             :: x_start, x_end
     integer(8)                          :: n_new, n_old
 
     grid_resolution = grid_resolution * 0.5d0
@@ -146,17 +148,17 @@ end subroutine
 
 
 logical function check_convergence(y_prime_old, y_prime_new, abs_tol, rel_tol) result(convergence)
-    real(8), allocatable, intent(in)    :: y_prime_old(:), y_prime_new(:)
-    real(8)                             :: combined_error, epsilon_val
+    real(dp), allocatable, intent(in)    :: y_prime_old(:), y_prime_new(:)
+    real(dp)                             :: combined_error, epsilon_val
     integer(8)                          :: new_size, old_size
-    real(8), intent(in)                 :: abs_tol, rel_tol
+    real(dp), intent(in)                 :: abs_tol, rel_tol
 
 
     new_size = size(y_prime_new)
     old_size = size(y_prime_old)
 
     convergence = .false.
-    combined_error = maxval( abs(y_prime_new(3:new_size-2:2) - y_prime_old(2:old_size-1) ) / (abs_tol + rel_tol * abs((y_prime_old(2:old_size-1))+ epsilon(epsilon_val) )) )
+    combined_error = maxval( abs(y_prime_new(3:new_size-2:2) - y_prime_old(2:old_size-1) ) / (abs_tol + rel_tol * abs((y_prime_old(2:old_size-1)) )) )
     print*, "Convergence: ", combined_error
 
     if (combined_error < 1d0) then
@@ -169,16 +171,16 @@ end function
 elemental function vector_function(x_in) result(f)
     implicit none
 
-    real(8), intent(in) :: x_in!(:)
-    real(8)             :: f!(size(x_in))
+    real(dp), intent(in) :: x_in!(:)
+    real(dp)             :: f!(size(x_in))
 
     f = sin(x_in**2)
      
 end function vector_function
 
 subroutine quick_plot(x, y1, y2)
-    real(8), intent(in)     :: x(:), y1(:), y2(:)
-    real(8), allocatable    :: y3(:)
+    real(dp), intent(in)     :: x(:), y1(:), y2(:)
+    real(dp), allocatable    :: y3(:)
     integer                 :: j, unit_dn
 
     !allocate(y3(size(x)))
@@ -203,8 +205,8 @@ end subroutine quick_plot
 subroutine make_grid(x_start, x_end, grid_resolution, grid_out)
     implicit none
 
-    real(8), intent(in)                 :: x_start, x_end, grid_resolution
-    real(8), intent(out), allocatable   :: grid_out(:)
+    real(dp), intent(in)                 :: x_start, x_end, grid_resolution
+    real(dp), intent(out), allocatable   :: grid_out(:)
     integer(8)                          :: j, number_of_points
 
 
@@ -222,9 +224,9 @@ end subroutine make_grid
 subroutine forward_difs(x, y, h, y_prime)
     implicit none
 
-    real(8), intent(in)                 :: x(:), y(:) 
-    real(8), allocatable, intent(out)   :: y_prime(:)
-    real(8)                             :: h
+    real(dp), intent(in)                 :: x(:), y(:) 
+    real(dp), allocatable, intent(out)   :: y_prime(:)
+    real(dp)                             :: h
     integer(8)                          :: n_elements
     
     allocate(y_prime(size(x)))
@@ -242,14 +244,15 @@ end subroutine forward_difs
 subroutine central_difs(y, h, y_prime)
     implicit none
 
-    real(8), intent(in)                 :: y(:) 
-    real(8), allocatable, intent(out)   :: y_prime(:)
-    real(8)                             :: h
-    integer(8)                          :: n_elements
+    real(dp), intent(in)                 :: y(:) 
+    real(dp), allocatable, intent(out)   :: y_prime(:)
+    real(dp)                             :: h, inv_2h
+    integer(8)                           :: n_elements
     
 
     n_elements = size(y)
     allocate(y_prime(n_elements))
+
 
     ! first element to calculate is i=2. so we need i= 3 and i=1. Last one is i = n-1 so we need i=n and i=n-2
     y_prime(2:n_elements-1) = (y(3:n_elements) - y(:n_elements-2)) / (2d0*h)
